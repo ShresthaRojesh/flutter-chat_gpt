86bebfa712020de8cfa27b125fcc522b077ebbae
ShresthaRojesh <rojeshshrestha135@hotmail.com>
2023-06-13 20:42:55 +0545

    Initial commit

---
+++ lib/widgets/text_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget(
+    {Key? key,
+    required this.label,
+    this.fontsize = 18,
+    this.color,
+    this.fontWeight, required int fontSize}):super(key:key);
+    
+final String label;
+final double fontsize;
+final Color? color;
+final FontWeight? fontWeight;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      label,
+      style: TextStyle(
+        color: color ?? Colors.white,
+        fontSize: fontsize,
+        fontWeight: fontWeight ?? FontWeight.w500,
+      ),
+    );
+  }
+}
